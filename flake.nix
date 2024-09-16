954cb7ec2c8412bae3edbef3485586329fe5cb11
ezekielenns <ezekiel.enns@protonmail.com>
2024-09-16 15:24:22 -0600

    got it all working but restore which is fine

--- flake.nix
+++ flake.nix
@@ -31,8 +31,8 @@
           shellHook = ''
             alias _Run='java -Xmx2G -jar fabric-server-mc.1.21.1-loader.0.16.5-launcher.1.0.1.jar nogui'
             alias _CleanUsers="rm -rf world/playerdata/**"
-#TODO
-            aliad _BackupData=' brotli -d outputfile.tar.br -o - | tar -xf -'
+            alias _BackupData='rm world.tar.br | tar -cvf - world | brotli -Z -o world.tar.br'
+            alias _restore='brotli -d world.tar.br | tar xvf world.tar --overwrite'
           '';
         };
       }
